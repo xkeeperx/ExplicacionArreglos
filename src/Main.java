5f942f118cdbd156b1c1e0ce4914b6448657f30b
Keeper Code <xkeeperx@gmail.com>
2021-09-09 23:48:09 -0500

    Update Main.java
    correcion

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Date fecha = new Date();
-        System.out.println("DIASSSSSs "+fecha.getTime());
+        System.out.println("DIA "+fecha.getTime());
         System.out.println("Mes "+fecha.getMonth());
         System.out.println("Mes2 "+fecha.getMonth());
     }
