015117ef540a4a4eae8676c065d3827e7df7aa01
Oscar Julian <xkeeperx@gmail.com>
2021-09-09 23:41:31 -0500

    Commit inicla.. creacion de archivos

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date fecha = new Date();
+        System.out.println("Mes "+fecha.getTime());
+        System.out.println("Mes "+fecha.getMonth());
+    }
+}
