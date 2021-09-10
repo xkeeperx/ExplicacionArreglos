75cb7d46db73b78be5d2e7105ac780b7a910111b
Oscar Julian <xkeeperx@gmail.com>
2021-09-09 23:44:26 -0500

    dadsfasdf

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Date fecha = new Date();
-        System.out.println("Mes "+fecha.getTime());
+        System.out.println("DIASSSSSs "+fecha.getTime());
         System.out.println("Mes "+fecha.getMonth());
     }
 }
